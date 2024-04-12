bbe3cc4bb4df946c869b17c26387b223c915624e
Justin Rinehuls <jrinehuls@gmail.com>
2024-04-11 20:56:52 -0400

    Register user and hash password in AuthRepo

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Text;
+using System.Security.Cryptography;
+
+namespace RPG_API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+            if (await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = $"User with username {user.Username} already exists";
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            // I think this is a shit way to do this
+            if (await _context.Users.AnyAsync(user => user.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Out parameters are set by this method to the variables where this method is invoked
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            HMACSHA512 hmac = new();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -2,5 +2,8 @@
 {
     public interface IAuthRepository
     {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
     }
 }
